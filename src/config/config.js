31251f9ed9e7dc5bfdf34d938a3c98cf4682bd4b
jobrtech <104224180+jobrtech@users.noreply.github.com>
2023-11-18 19:28:00 +0500

    aws url added

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://visautoml-be-a3ac6ac86f0a.herokuapp.com";
-export const BACKEND_BASE_URL = "https://visautoml-be-a3ac6ac86f0a.herokuapp.com/api/";
-export const BACKEND_BASE_FLASK_URL = "https://visautoml-be-a3ac6ac86f0a.herokuapp.com:8050";
+export const API_BASE_URL = "http://visautoml-env.eba-6xwebzkh.ap-southeast-2.elasticbeanstalk.com";
+export const BACKEND_BASE_URL = "http://visautoml-env.eba-6xwebzkh.ap-southeast-2.elasticbeanstalk.com/api/";
+export const BACKEND_BASE_FLASK_URL = "http://visautoml-env.eba-6xwebzkh.ap-southeast-2.elasticbeanstalk.com:8050";
