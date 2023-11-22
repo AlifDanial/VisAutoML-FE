94c03b3cfc466c9bc60531ddce7c7199b512f994
Muhammad Alif Danial <43855587+AlifDanial@users.noreply.github.com>
2023-11-22 15:38:32 +0800

    url

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_BASE_URL = "https://4a76-115-134-225-27.ngrok-free.app";
+export const BACKEND_BASE_URL = "https://4a76-115-134-225-27.ngrok-free.app/api";
 export const BACKEND_BASE_FLASK_URL = "https://e0af-115-134-225-27.ngrok-free.app";
 export const API_BASE_URL = "https://4a76-115-134-225-27.ngrok-free.app";
