e8b06d48c804ecce5ba61054ffe3174007436177
Alif <aldmnsri@gmail.com>
2024-01-09 15:48:19 +0800

    Sandro Link

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_BASE_URL = "https://4a76-115-134-225-27.ngrok-free.app/api/";
-export const BACKEND_BASE_FLASK_URL = "https://e0af-115-134-225-27.ngrok-free.app";
-export const API_BASE_URL = "https://4a76-115-134-225-27.ngrok-free.app";
+export const BACKEND_BASE_URL = "https://1ae8-14-192-208-203.ngrok-free.app/api/";
+export const BACKEND_BASE_FLASK_URL = "https://9a99-14-192-208-203.ngrok-free.app";
+export const API_BASE_URL = "https://1ae8-14-192-208-203.ngrok-free.app";
