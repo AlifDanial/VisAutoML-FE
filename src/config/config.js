87a5b448572c1de686e2d97f800d6a1a53a83970
Muhammad Alif Danial <aldmnsri@gmail.com>
2024-09-30 12:51:16 +0800

    Grant Links

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_BASE_URL = "https://1ae8-14-192-208-203.ngrok-free.app/api/";
-export const BACKEND_BASE_FLASK_URL = "https://9a99-14-192-208-203.ngrok-free.app";
-export const API_BASE_URL = "https://1ae8-14-192-208-203.ngrok-free.app";
+export const BACKEND_BASE_URL = "https://3d3b-113-211-210-232.ngrok-free.app/api/";
+export const BACKEND_BASE_FLASK_URL = "https://9869-113-211-210-232.ngrok-free.app";
+export const API_BASE_URL = "https://3d3b-113-211-210-232.ngrok-free.app";
